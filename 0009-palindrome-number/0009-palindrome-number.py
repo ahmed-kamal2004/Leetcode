2cf5f7e244c2f0634c2017ca590adb1b72c456fe
Ahmed Kamal <98265644+ahmed-kamal2004@users.noreply.github.com>
2024-01-01 23:47:34 +0200

    Time: 117 ms (5.02%), Space: 17.4 MB (7.11%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -3,13 +3,20 @@ class Solution:
         if x<0:
             return False
         else:
-            x = str(x)
+
+            lista = []
+            while x!=0:
+                lista.append(x%10)
+                x = x//10
+
             start = 0
-            end = len(x)-1
-            while start!=end-start and  end-start> start:
-                if x[start] != x[len(x)-1-start]:
+            end = len(lista)-1
+
+            while start !=end and end > start:
+                if lista[start] != lista[end]:
                     return False
-                else:
-                    start+=1
+                start +=1
+                end -=1
+
             return True
 
